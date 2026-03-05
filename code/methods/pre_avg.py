a344298bbddbb11cb67f5df9e041aade7b0f49ce
aadnero <aadnero@ulrik.uio.no>
2026-03-05 10:01:44 +0100

    Implement pre_avg

--- code/methods/pre_avg.py
+++ code/methods/pre_avg.py
@@ -1,17 +1,29 @@
 import numpy as np
 import pandas as pd
 
-def pre_avg(df: pd.DataFrame, 
+def pre_avg(df: pd.DataFrame,
+            column: str, 
             tick: bool = True, 
             k: int = 5,
-            time_interval_ms: int = 100) -> np.ndarray:
-    if tick == True:
-        pass
-    else: 
-        pass 
+            time_interval_ms: int = 100, 
+            time_col: str = "datetime") -> pd.Dataframe:
+    
+    df = df.copy()
+    
+    if tick:
+        df["pre_avg"] = df[column].rolling(window=k).mean()
+    else:
+        df[time_col] = pd.to_datetime(df[time_col]) 
+        
+        df = df.set_index(time_col)
+        
+        df["pre_avg"] = (
+            df[column]
+            .resample(f"{time_interval_ms}ms")
+            .transform("mean")
+        )
 
-#### Make function for pre-averaging: 
-# P^bar_t = 1/k sum_{j=0}^{j=k}(P_{k-j})
+        df = df.reset_index()
+        
+    return df
 
-
-# Se på dette
