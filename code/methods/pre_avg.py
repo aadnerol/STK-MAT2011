6347f5f10b9f5c1a3aefc25850a7ded1c2db38f6
aadnero <aadnero@ulrik.uio.no>
2026-03-05 10:05:18 +0100

    Docstring

--- code/methods/pre_avg.py
+++ code/methods/pre_avg.py
@@ -7,6 +7,22 @@ def pre_avg(df: pd.DataFrame,
             k: int = 5,
             time_interval_ms: int = 100, 
             time_col: str = "datetime") -> pd.Dataframe:
+    """Function for pre-averaging scheme.
+
+    Args:
+        df (pd.DataFrame): Tick data
+        column (str): Name of column containing prices to be pre-averaged
+        tick (bool, optional): Pre-average on tick. Defaults to True. If false, 
+            pre-average on time
+        k (int, optional): Size of pre-averaging block. Defaults to 5.
+        time_interval_ms (int, optional): Length of pre-averaging time block. 
+            Defaults to 100.
+        time_col (str, optional): Name of column containing date/time. 
+            Defaults to "datetime".
+
+    Returns:
+        pd.Dataframe: Dataframe with added "pre_avg" column
+    """
     
     df = df.copy()
     
