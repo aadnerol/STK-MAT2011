c449e3f1c4017a601cb0d6aa117a7079405d3104
aadnero <aadnero@ulrik.uio.no>
2026-03-05 09:49:19 +0100

    Update pre_avg.py

--- code/methods/pre_avg.py
+++ code/methods/pre_avg.py
@@ -1,5 +1,17 @@
-def pre_avg():
-    pass
+import numpy as np
+import pandas as pd
+
+def pre_avg(df: pd.DataFrame, 
+            tick: bool = True, 
+            k: int = 5,
+            time_interval_ms: int = 100) -> np.ndarray:
+    if tick == True:
+        pass
+    else: 
+        pass 
 
 #### Make function for pre-averaging: 
 # P^bar_t = 1/k sum_{j=0}^{j=k}(P_{k-j})
+
+
+# Se på dette
